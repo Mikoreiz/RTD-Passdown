e8d683fe351c432d16b8adbe603ac28b353944bd
Mikhael Oreiro <mikhaeloreiro@yahoo.com>
2019-02-28 00:39:22 -0800

    Added post request

--- index.js
+++ index.js
@@ -4,43 +4,48 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/rtd-passdown',{useNewUrlParser: true});
-
-var Bus = require('./model/bus');
+var Schema = mongoose.Schema;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,'index.html'));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname,'index.html'));
+});
+
+var bus = new Schema({
+	busNumber : Number,
+	type: String,
+	date : String,
+	noPart: String,
+	reason : String,
+	description : String,
+});
+
+var addBus = mongoose.model('holdbus', bus);
+
+app.post('/submitBus', function(request,response){
+	new addBus({
+		busNumber : request.body.inputBus,
+		type : request.body.inputType,
+		date : request.body.inputDate,
+		noPart : request.body.inputNoPart,
+		reason : request.body.inputReason,
+		description : request.body.inputDesc
+	}).save(function(err){
+        if(err){
+            response.status(500).send({error:"Could not save product"});
+        } else {
+            console.log('your form has been saved');
+        }
+        response.sendFile(path.join(__dirname,'index.html'));
+    });
 });
 
 app.listen(3000, function() {
-	console.log("Passdown running on port 3000...");
+console.log("Passdown running on port 3000...");
 });
 
-app.post('/bus', function(request, response){
-	var bus = new Bus();
-	bus.busNumber = request.body.busNumber;
-	bus.type = request.body.type;
-	bus.date = request.body.date;
-	bus.noPart = request.body.noPart;
-	bus.reason = request.body.reason;
-	bus.description = request.body.description;
-	bus.save(function(err, savedBus){
-		if (err) {
-			response.status(500).send({error:"Could not save bus"});
-		} else {
-			response.send(savedBus);
-		}
-	});
-});
 
-app.get('/bus', function(request, response) {
-	Bus.find({}, function(err, buses){
-		if (err){
-			response.status(500).send({error:"Could not fetch bus"});
-		} else {
-			response.send(buses);
-		}
-	});
-});
+
